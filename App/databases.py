a8df1e6ec01a3c962a03377c307a7ac7699c91b6
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-02-01 01:30:42 +0100

    added new files

--- App/databases.py
+++ App/databases.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-sql_alchemy_database_url ='sqlite:///./blog.db'
+sql_alchemy_database_url ='sqlite:///blog.db'
 
 engine = create_engine(sql_alchemy_database_url,connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
