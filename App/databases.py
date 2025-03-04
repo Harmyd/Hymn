75d1c0e172bfe7cbaeda4b8799225d92b90f4e5e
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-03-04 23:29:10 +0100

    modified

--- App/databases.py
+++ App/databases.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-sql_alchemy_database_url ='sqlite:///blog.db'
+sql_alchemy_database_url ='postgresql://hymn:Mgp8soPw9WsAwWVGW2EFHjxKO7gXy4wb@dpg-cv3m1qvnoe9s738mm5sg-a.oregon-postgres.render.com/hymn_db'
 
-engine = create_engine(sql_alchemy_database_url,connect_args={"check_same_thread":False})
+engine = create_engine(sql_alchemy_database_url)
 SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
 
 Base= declarative_base()
